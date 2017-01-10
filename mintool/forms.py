fa0c28b12d3afb6e64abe164f2fdf25a8be84da5
Sebastian Bultmann <s.bultmann@me.com>
2017-01-10 16:55:16 +0100

    almost done  - fix last bugs

--- mintool/forms.py
+++ mintool/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class RequestForm(forms.Form):
     CHOICES = (('Mouse', 'Mouse'),('Human', 'Human'),)
     gene_name = forms.CharField(label='gene name', max_length=100)
-    organism = forms.CharField(label='organism',widget=forms.Select(choices=CHOICES))
+    organism = forms.CharField(label='organism', widget=forms.Select(choices=CHOICES))
 
--- mintool/functions/entrez.py
+++ mintool/functions/entrez.py
@@ -9,76 +9,120 @@ def query_ncbi(gene_name, org_name):
 
     handle = Entrez.esearch(db="gene", term="%s[Gene Name] AND %s[Organism]" % (gene_name,org_name))
     record = Entrez.read(handle)
-    ids = record["IdList"][0]
-
-    handle = Entrez.efetch(db="gene", id=ids, rettype="gb", retmode="xml")
-    records = Entrez.parse(handle)
     result = []
-    for record in records:
-        x = record['Entrezgene_locus'][0]['Gene-commentary_products']
-        gene_ref = record['Entrezgene_gene']['Gene-ref']['Gene-ref_locus']
-        gene_syn = record['Entrezgene_gene']['Gene-ref']['Gene-ref_syn']
-        locus_accession = record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']\
-            ['Seq-interval']['Seq-interval_id']['Seq-id']['Seq-id_gi']
-        locus_toT =record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']\
-            ['Seq-interval']['Seq-interval_to']
-        locus_fromT = record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int'] \
-            ['Seq-interval']['Seq-interval_from']
-        locus_to = max([int(locus_toT), int(locus_fromT)])
-        locus_from = min([int(locus_toT), int(locus_fromT)])
+    try:
+        ids = record["IdList"][0]
 
-        locus_strand = str(record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int'] \
-            ['Seq-interval']['Seq-interval_strand']).split(":")[2].split("}")[0][2:-1]
-        if locus_strand == "minus":
-            locus_strand = 2
-        else:
-            locus_strand = 1
-        locus_chr = record['Entrezgene_locus'][0]['Gene-commentary_label']
-        try:
-            locus_des = record['Entrezgene_summary']
-        except:
-            locus_des = "No description available"
-        iso_info_list = []
-        pxwidth = 300
-        maxlenght = max([int(locus_to), int(locus_from)])
-        width = max([int(locus_to), int(locus_from)]) - min([int(locus_to), int(locus_from)])
-        ratio = pxwidth/width
-        for i in x:
-            if 'Gene-commentary_products' in i.keys() and "X" not in i['Gene-commentary_label']:
-                exons = []
-                real_exons=[]
-                isoform_name = i['Gene-commentary_label']
-                isoform_accessionP = i['Gene-commentary_products'][0]['Gene-commentary_accession']
-                isoform_accessionN = i['Gene-commentary_seqs'][0]['Seq-loc_whole']['Seq-id']['Seq-id_gi']
-                isoform_locs = i['Gene-commentary_products'][0]['Gene-commentary_genomic-coords']\
-                    [0]['Seq-loc_mix']['Seq-loc-mix']
-                for loc in isoform_locs:
-                    loc_strand = str(loc['Seq-loc_int']['Seq-interval']['Seq-interval_strand']).split(":")[2].split("}")[0][2:-1]
-                    if loc_strand == "plus":
-                        cor = 300
-                    else:
-                        cor = 0
-                    loc_to = int(loc['Seq-loc_int']['Seq-interval']['Seq-interval_to'])
-                    loc_from = int(loc['Seq-loc_int']['Seq-interval']['Seq-interval_from'])
-                    real_exons.append(loc_to)
-                    real_exons.append(loc_from)
-                    loc_to = abs(int((float(loc['Seq-loc_int']['Seq-interval']['Seq-interval_to'])-maxlenght)*ratio)+cor)
-                    loc_from = abs(int((float(loc['Seq-loc_int']['Seq-interval']['Seq-interval_from'])-maxlenght)*ratio)+cor)
+        handle = Entrez.efetch(db="gene", id=ids, rettype="gb", retmode="xml")
+        records = Entrez.parse(handle)
 
-                    #if loc_to > loc_from:
-                    exon = [loc_to, loc_from]
-                    #else:
-                     #   exon = [loc_from, loc_to]
-                    exons.append(exon)
-                seqDict = extract_sequences(locus_accession,min(real_exons), max(real_exons), locus_strand)
-                iso_info_list.append(json.dumps({"Name": isoform_name, "Accession_protein": isoform_accessionP,
-                                                 "Accession_nucleotide": isoform_accessionN, "Exons": exons,
-                                                 "Nterm": seqDict["Nterm"], "Cterm" : seqDict["Cterm"]}))
+        for record in records:
+            x = record['Entrezgene_locus'][0]['Gene-commentary_products']
+            gene_ref = record['Entrezgene_gene']['Gene-ref']['Gene-ref_locus']
+            gene_syn = record['Entrezgene_gene']['Gene-ref']['Gene-ref_syn']
+            locus_accession = record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']\
+                ['Seq-interval']['Seq-interval_id']['Seq-id']['Seq-id_gi']
+            locus_toT =record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']\
+                ['Seq-interval']['Seq-interval_to']
+            locus_fromT = record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int'] \
+                ['Seq-interval']['Seq-interval_from']
+            locus_to = max([int(locus_toT), int(locus_fromT)])
+            locus_from = min([int(locus_toT), int(locus_fromT)])
 
-    result.append({"Gene_Name": gene_ref, "chr": re.findall(r"[0-9XY]+", locus_chr)[0],"Gene_Syn": gene_syn, "Accession": locus_accession, "To": locus_to,
-                   "From": locus_from, "Strand": locus_strand, "Chr": locus_chr, "Description": locus_des,
-                   "Isoforms": iso_info_list})
+            locus_strand = str(record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int'] \
+                ['Seq-interval']['Seq-interval_strand']).split(":")[2].split("}")[0][2:-1]
+            if locus_strand == "minus":
+                locus_strand = 2
+            else:
+                locus_strand = 1
+            locus_chr = record['Entrezgene_locus'][0]['Gene-commentary_label']
+            try:
+                locus_des = record['Entrezgene_summary']
+            except:
+                locus_des = "No description available"
+            iso_info_list = []
+            pxwidth = 300
+            maxlenght = max([int(locus_to), int(locus_from)])
+            width = max([int(locus_to), int(locus_from)]) - min([int(locus_to), int(locus_from)])
+            ratio = pxwidth/width
 
+            for i in x:
+
+                try:
+                    if 'Gene-commentary_products' in i.keys() and "X" not in i['Gene-commentary_label']:
+                        exons = []
+                        real_exons=[]
+                        isoform_name = i['Gene-commentary_label']
+                        isoform_accessionP = i['Gene-commentary_products'][0]['Gene-commentary_accession']
+                        isoform_accessionN = i['Gene-commentary_seqs'][0]['Seq-loc_whole']['Seq-id']['Seq-id_gi']
+                        isoform_locs = i['Gene-commentary_products'][0]['Gene-commentary_genomic-coords']\
+                            [0]['Seq-loc_mix']['Seq-loc-mix']
+                        for loc in isoform_locs:
+                            loc_strand = str(loc['Seq-loc_int']['Seq-interval']['Seq-interval_strand']).split(":")[2].split("}")[0][2:-1]
+                            if loc_strand == "plus":
+                                cor = 300
+                            else:
+                                cor = 0
+                            loc_to = int(loc['Seq-loc_int']['Seq-interval']['Seq-interval_to'])
+                            loc_from = int(loc['Seq-loc_int']['Seq-interval']['Seq-interval_from'])
+                            real_exons.append(loc_to)
+                            real_exons.append(loc_from)
+                            loc_to = abs(int((float(loc['Seq-loc_int']['Seq-interval']['Seq-interval_to'])-maxlenght)*ratio)+cor)
+                            loc_from = abs(int((float(loc['Seq-loc_int']['Seq-interval']['Seq-interval_from'])-maxlenght)*ratio)+cor)
+
+                            #if loc_to > loc_from:
+                            exon = [loc_to, loc_from]
+                            #else:
+                             #   exon = [loc_from, loc_to]
+                            exons.append(exon)
+                        seqDict = extract_sequences(locus_accession,min(real_exons), max(real_exons), locus_strand)
+                        iso_info_list.append(json.dumps({"Name": isoform_name, "Accession_protein": isoform_accessionP,
+                                                         "Accession_nucleotide": isoform_accessionN, "Exons": exons,
+                                                         "Nterm": seqDict["Nterm"], "Cterm" : seqDict["Cterm"]}))
+                except:
+                    print(i.keys())
+                    print(i)
+                    if 'Gene-commentary_products' in i.keys():
+                        exons = []
+                        real_exons=[]
+                        isoform_name = i['Gene-commentary_accession']
+                        isoform_accessionP = i['Gene-commentary_products'][0]['Gene-commentary_accession']
+                        isoform_accessionN = i['Gene-commentary_seqs'][0]['Seq-loc_whole']['Seq-id']['Seq-id_gi']
+                        print(i['Gene-commentary_products'][0]['Gene-commentary_genomic-coords'])
+                        try:
+                            isoform_locs = i['Gene-commentary_products'][0]['Gene-commentary_genomic-coords']\
+                                [0]['Seq-loc_mix']['Seq-loc-mix']
+                        except:
+                            isoform_locs = i['Gene-commentary_products'][0]['Gene-commentary_genomic-coords']
+                        for loc in isoform_locs:
+                            loc_strand = str(loc['Seq-loc_int']['Seq-interval']['Seq-interval_strand']).split(":")[2].split("}")[0][2:-1]
+                            if loc_strand == "plus":
+                                cor = 300
+                            else:
+                                cor = 0
+                            loc_to = int(loc['Seq-loc_int']['Seq-interval']['Seq-interval_to'])
+                            loc_from = int(loc['Seq-loc_int']['Seq-interval']['Seq-interval_from'])
+                            real_exons.append(loc_to)
+                            real_exons.append(loc_from)
+                            loc_to = abs(int((float(loc['Seq-loc_int']['Seq-interval']['Seq-interval_to'])-maxlenght)*ratio)+cor)
+                            loc_from = abs(int((float(loc['Seq-loc_int']['Seq-interval']['Seq-interval_from'])-maxlenght)*ratio)+cor)
+
+                            #if loc_to > loc_from:
+                            exon = [loc_to, loc_from]
+                            #else:
+                             #   exon = [loc_from, loc_to]
+                            exons.append(exon)
+                        seqDict = extract_sequences(locus_accession,min(real_exons), max(real_exons), locus_strand)
+                        iso_info_list.append(json.dumps({"Name": isoform_name, "Accession_protein": isoform_accessionP,
+                                                         "Accession_nucleotide": isoform_accessionN, "Exons": exons,
+                                                         "Nterm": seqDict["Nterm"], "Cterm" : seqDict["Cterm"]}))
+        result.append({"Gene_Name": gene_ref, "chr": re.findall(r"[0-9XY]+", locus_chr)[0],"Gene_Syn": gene_syn,
+                       "Accession": locus_accession, "To": locus_to,
+                       "From": locus_from, "Strand": locus_strand, "Chr": locus_chr, "Description": locus_des,
+                       "Isoforms": iso_info_list})
+    except:
+        result.append({"Gene_Name": "Oops!", "Description": "<b>No hits found! Check for spelling mistakes or head over to "
+                        "<a href='https://www.ncbi.nlm.nih.gov/gene'>NCBI</a> to check if the gene name is correct</b>"})
     return result
 
 
@@ -184,8 +228,8 @@ def gRNA_oligos(seq):
     r_oligo = "aaac"+str(seq.reverse_complement())[3:]+"c"
     return (f_oligo, r_oligo)
 
-#print(query_ncbi("Dnmt3a", "Mouse"))
-
+#print(query_ncbi("Uhrf2", "Mouse"))
+#print(query_ncbi("zfp41", "Mouse"))
 #print(extract_sequences(372099101,20907520,20941453,2))
 #print(extract_sequences(372099098,3835197,3907746,1))
 
