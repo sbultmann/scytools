6e4d8c4f64dd79fb4fcaabcee56aa5b9747de1dc
Sebastian Bultmann <s.bultmann@me.com>
2017-01-12 09:20:04 +0100

    really last commit

--- mintool/urls.py
+++ mintool/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^mintool/', views.index, name='index'),
     url(r'^(?P<gene_org>[0-9A-Za-z]+)/request/$', views.requestNCBI, name="request"),
     url(r'^[0-9A-Za-z]+/request/gRNA-design/$', views.gRNAdesign, name="gRNAdesign"),
     url(r'^[0-9A-Za-z]+/request/create_csv/$', views.create_csv, name="create_csv"),
